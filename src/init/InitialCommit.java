087759cac192e08900c03b51f8679dbb2b594dd0
cqlzx <bupt11lzx@163.com>
2017-08-20 23:33:41 -0500

    Initial Commit

---
+++ src/init/InitialCommit.java
@@ -0,0 +1,9 @@
+package init;
+
+public class InitialCommit {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, this is the initial commit for movie name search engine");
+	}
+
+}
